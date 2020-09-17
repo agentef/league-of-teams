dfc6ea648be69170e681f9f1485a4b2c37e860d1
Felipe Leite Uematsu <felipe.uematsu@digio.com.br>
2020-09-17 00:09:49 -0300

    init

--- lib/view/general/background.dart
+++ lib/view/general/background.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Background extends StatelessWidget {
+  Background(this.widget);
+
+  final Widget widget;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -8,14 +13,42 @@ class Background extends StatelessWidget {
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
       child: Stack(
-          children: [
-            Image(image: AssetImage("Background_Image")),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.80,
-              width: MediaQuery.of(context).size.width * 0.65,
-              color: Colors.white70,
-        ),
-      ]),
+        children: [
+
+          Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width / 2,
+                    fit: BoxFit.cover,
+                    image: AssetImage("background/spirit-blossom-kindred.jpg")),
+                Image(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width / 2,
+                    fit: BoxFit.cover,
+                    image: AssetImage("background/spirit-blossom-yasuo.jpg"))
+              ]),
+          Center(
+            child: ConstrainedBox(
+
+              constraints: BoxConstraints(
+                  minHeight: 900, minWidth: 860
+              ),
+              child: Container(
+                color: Colors.blueGrey,
+                height: MediaQuery.of(context).size.height * 0.9,
+                width: MediaQuery.of(context).size.width * 0.45,
+                child: Container(
+                  color: Colors.white70,
+                  child: widget,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/view/pages/home.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:league_team_analytics/view/general/main_container.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MainContainer();
+  }
+}
